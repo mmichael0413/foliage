6ba9bb64f48382fa216e5ba64b0da0153b5f13e2
Steve Pember <spember@gmail.com>
2014-10-14 16:11:41 -0400

    -very slight tweak to stores template

--- js/app/templates/hbs-compiled.js
+++ js/app/templates/hbs-compiled.js
@@ -1200,7 +1200,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (helper = helpers.uid) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.uid); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</a> ";
+    + "</a>, ";
   if (helper = helpers.city) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.city); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
