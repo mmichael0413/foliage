4fa524476de275e83d009a486de7ecdfc59d9d27
Andrew Croswell <andrew@thirdchannel.com>
2014-11-04 17:22:44 -0500

    Fixed TC-898

--- js/app/views/stores/intel/edit.js
+++ js/app/views/stores/intel/edit.js
@@ -5,12 +5,24 @@ define(function(require) {
 
     return Backbone.View.extend({
         el: '.store-intel',
+        events: {
+            "click [data-show-element]" : 'showElement',
+            "click [data-hide-element]" : 'hideElement'
+        },
         initialize: function (options) {
         },
         render :function () {
             this.$el.find('select').chosen({disable_search: true, width: "100%"});
             this.$el.find('textarea:visible').expanding();
             return this;
+        },
+        showElement: function (e, data) {
+            var $elem = this.$el.find(e.currentTarget);
+            this.$el.find($elem.data('show-element')).show('fast', "linear");
+        },
+        hideElement: function (e, data) {
+            var $elem = this.$el.find(e.currentTarget);
+            this.$el.find($elem.data('hide-element')).hide('fast', "linear").val('').trigger('change');
         }
     });
 });
