1894de15421b4fe2a07fb32af75c9dd33a8ad31e
Steve Pember <spember@gmail.com>
2014-11-04 21:06:09 -0500

    -tweaked some styles on the alerts alert resolution

--- js/app/templates/hbs-compiled.js
+++ js/app/templates/hbs-compiled.js
@@ -521,7 +521,7 @@ helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partial
   var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
 
-  buffer += "<p class=\"col-7-12 col-md-1\"><a href=\""
+  buffer += "<p class=\"col-7-12 col-md-9	-12\"><a href=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.profile)),stack1 == null || stack1 === false ? stack1 : stack1.link)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\" class=\"store-profile-link\">";
   if (helper = helpers.name) { stack1 = helper.call(depth0, {hash:{},data:data}); }
@@ -535,11 +535,9 @@ helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partial
   if (helper = helpers.dateAdded) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.dateAdded); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</p>\n<div class=\"col-1-12 col-md-1\">\n	<div class=\"action\">\n        <a href=\""
-    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.alert)),stack1 == null || stack1 === false ? stack1 : stack1.links)),stack1 == null || stack1 === false ? stack1 : stack1.self)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" class=\"btn primary expand\"><i class=\"ic fa ic_check\"></i><span class=\"visible-xs\">View Report</span></a>\n    </div>\n	<div class=\"action\">\n        <a href=\""
+    + "</p>\n<div class=\"col-1-12 col-md-3-12\">\n	\n        <a href=\"#\" class=\"btn primary expand\"><i class=\"ic fa ic_check\"></i><span class=\"visible-xs\">View Report</span></a>\n    \n        <a href=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.report)),stack1 == null || stack1 === false ? stack1 : stack1.link)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" class=\"btn primary\"><i class=\"ic fa ic_report-16\"></i><span class=\"visible-xs\">Resolve Alert</span></a>\n    </div>\n</div>\n\n";
+    + "\" class=\"btn primary\"><i class=\"ic fa ic_report-16\"></i><span class=\"visible-xs\">Resolve Alert</span></a>\n    \n</div>\n\n";
   stack1 = self.invokePartial(partials.alert_details_empty_row, 'alert_details_empty_row', depth0, helpers, partials, data);
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n\n";
