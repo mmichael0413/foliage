a4919d24d2979ce836b4a8e5e53345fafa3c630d
Matthew Little <matt@thethirdchannel.com>
2014-11-07 13:28:14 -0500

    fixing a unneeded escape character in a regex

--- js/app/views/layout/navigation.js
+++ js/app/views/layout/navigation.js
@@ -193,7 +193,7 @@ define(function (require) {
         if (navigator.appName == 'Microsoft Internet Explorer')
         {
             var ua = navigator.userAgent;
-            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+            var re  = new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");
             if (re.exec(ua) != null)
                 rv = parseFloat( RegExp.$1 );
         }
