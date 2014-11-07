5af1192a5c6aa3ca86ac1a8669a62abd471ec951
Andrew Croswell <andrew@thirdchannel.com>
2014-11-07 16:48:21 -0500

    Added in some status for the dashboard page as it's loading

--- js/app/views/dashboards/alerts/index/sections.js
+++ js/app/views/dashboards/alerts/index/sections.js
@@ -3,7 +3,8 @@ define(function(require) {
         SectionsCollection = require('app/collections/dashboards/alerts/sections'),
         SectionView = require('app/views/dashboards/alerts/index/section'),
         FiltersCollection = require('app/collections/dashboards/alerts/index/filters'),
-        Filter = require('app/views/filter/main');
+        Filter = require('app/views/filter/main'),
+        LoadingView = require('app/views/utils/loading');
 
     return Backbone.View.extend({
         className: "alerts-sections",
@@ -11,14 +12,19 @@ define(function(require) {
             this.options = options;
             this.collection = new SectionsCollection({programId: options.programId});
             this.filters = new FiltersCollection({programId: options.programId});
+            this.loadingView = new LoadingView();
         },
         render: function () {
             var self = this;
+
+            this.$el.html(this.loadingView.render().$el);
+
             this.filters.fetch({success: function (collection) {
                 self.addFilters(collection);
             }});
 
             this.collection.fetch({success: function (collection) {
+                self.loadingView.remove();
                 collection.each(function (model) {
                     self.$el.append(new SectionView({programId: self.options.programId, model: model}).render().$el);
                 });
