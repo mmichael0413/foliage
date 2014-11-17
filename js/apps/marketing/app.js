0926511b3f80fdc6ac13afb2373a512d68d6dfc6
Scott <s.pullen05@gmail.com>
2014-11-17 11:23:31 -0500

    adding full section with control to move to next section

--- js/apps/marketing/app.js
+++ js/apps/marketing/app.js
@@ -4,7 +4,16 @@ define(function(require){
 
     return {
         initialize: function () {
+            $('.full-section-controls > a').on('click', function(e) {
+                e.preventDefault();
+                var $el = $(this);
+                var $parent = $el.parent().parent();
+                var $next = $parent.next('.full-section');
 
+                $('body').animate({
+                    scrollTop: $next.offset().top - 70
+                }, 1000);
+            });
         }
     };
 });
