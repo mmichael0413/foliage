696d7f80d7189a614f6f285eeb1415d8e10e3460
Steve Pember <spember@gmail.com>
2015-02-02 16:35:42 -0500

    -added functionality for submitting mileage calculations for pennypacker

--- js/apps/pennyPacker/views/entries/actions.js
+++ js/apps/pennyPacker/views/entries/actions.js
@@ -1,6 +1,7 @@
 define(function(require) {
     var Backbone = require('backbone'),
         context = require('context'),
+        $ = require('jquery'),
 
         /**
          * Encapsules the Actions Row in the Entries List View, specifically the buttons regarding report generation
@@ -11,13 +12,30 @@ define(function(require) {
             el: '.content',
 
             events: {
-                'click .paypal': 'paypal'
+                'click .paypal': 'paypal',
+                'click .mileage': 'calculateMileage'
             },
 
             paypal: function (e) {
                 e.stopPropagation();
                 e.preventDefault();
                 window.location = context.content.links.paypal + window.location.search + "&format=csv";
+            },
+            calculateMileage: function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+                var $link = $(e.currentTarget),
+                    url = $link.attr('href') + window.location.search,
+                    $spinner = $("<i class='fa fa-spinner fa-spin'></i>");
+                
+                $.getJSON(
+                    url,
+                    {}
+                )
+                .done( function (results) {
+                    
+                });
+                
             }
 
 
