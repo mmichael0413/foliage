ce9e3d12eda4656541fadb6d81991e00f703528c
Scott Pullen <s.pullen05@gmail.com>
2015-03-18 11:48:08 -0400

    cleaning up the duplication of operateOnPaths function

--- tasks/requirejs.js
+++ tasks/requirejs.js
@@ -8,35 +8,12 @@
 
 /**
  *
- * Wrapper to perform an operation on each matching path item. Could be a file or a directory.
- * Each item will be passed to a callback - fn - which will receive the following arguments:
- * - the name of the item (e.g. the last position in the file path)
- * - the full path
- * - the tokenized breakup of the path
- *
- */
-function operateOnPaths(grunt, path, fn) {
-    var appPaths = grunt.file.expand(path),
-        tokens,
-        name,
-        i = appPaths.length;
-
-    while(i--) {
-        tokens = appPaths[i].split('/');
-        // callback that fn utilizes
-        // name, path, tokens
-        fn(tokens[tokens.length-1], appPaths[i], tokens);
-    }
-}
-
-/**
- *
  * Builds the requirejs task blocks. Generally one for each app / service
  *
  */
 function buildRequireJSConfig(grunt) {
     var config = {};
-    operateOnPaths(grunt, 'js/apps/*', function (name, path, tokens) {
+    grunt.util.operateOnPaths(grunt, 'js/apps/*', function (name, path, tokens) {
         // checking if the path exists, so that we can have additional 'apps' containing shared modules
         // which we don't necessarily want to build
         if (grunt.file.exists(path + '/init.js')) {
--- tasks/sass.js
+++ tasks/sass.js
@@ -7,30 +7,6 @@
 "use strict";
 
 /**
- * TODO: Figure out if there's a way to make this accessible to all tasks
- *
- * Wrapper to perform an operation on each matching path item. Could be a file or a directory.
- * Each item will be passed to a callback - fn - which will receive the following arguments:
- * - the name of the item (e.g. the last position in the file path)
- * - the full path
- * - the tokenized breakup of the path
- *
- */
-function operateOnPaths(grunt, path, fn) {
-    var appPaths = grunt.file.expand(path),
-        tokens,
-        name,
-        i = appPaths.length;
-
-    while(i--) {
-        tokens = appPaths[i].split('/');
-        // callback that fn utilizes
-        // name, path, tokens
-        fn(tokens[tokens.length-1], appPaths[i], tokens);
-    }
-}
-
-/**
  * Builds the Sass configuration for each app
  *
  * @param  {grunt} grunt The grunt object
@@ -38,7 +14,7 @@ function operateOnPaths(grunt, path, fn) {
  */
 function buildSassConfig(grunt) {
     var files = {};
-    operateOnPaths(grunt, 'css/scss/apps/*', function (name, path, tokens) {
+    grunt.util.operateOnPaths(grunt, 'css/scss/apps/*', function (name, path, tokens) {
         if (grunt.file.exists(path + '/main.scss')) {
             files['dist/' + name + '/css/main.css'] = path + '/main.scss';
         } else {
