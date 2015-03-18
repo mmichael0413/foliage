cbb727d3d94a0de8735490b64beb46a1e99d6f00
Scott Pullen <s.pullen05@gmail.com>
2015-03-18 11:49:08 -0400

    more clean up of the duplication of operateOnPaths function

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,32 +10,18 @@
  * Link: https://github.com/firstandthird/load-grunt-config
  */
 
-/**
- *
- * Wrapper to perform an operation on each matching path item. Could be a file or a directory.
- * Each item will be passed to a callback - fn - which will receive the following arguments:
- * - the name of the item (e.g. the last position in the file path)
- * - the full path
- * - the tokenized breakup of the path
- *
- */
-function operateOnPaths(grunt, path, fn) {
-    var appPaths = grunt.file.expand(path),
-        tokens,
-        name,
-        i = appPaths.length;
-
-    while(i--) {
-        tokens = appPaths[i].split('/');
-        // callback that fn utilizes
-        // name, path, tokens
-        fn(tokens[tokens.length-1], appPaths[i], tokens);
-    }
-}
-
 module.exports = function (grunt) {
     "use strict";
 
+    /**
+     *
+     * Wrapper to perform an operation on each matching path item. Could be a file or a directory.
+     * Each item will be passed to a callback - fn - which will receive the following arguments:
+     * - the name of the item (e.g. the last position in the file path)
+     * - the full path
+     * - the tokenized breakup of the path
+     *
+     */
     grunt.util.operateOnPaths = function(grunt, path, fn) {
         var appPaths = grunt.file.expand(path),
             tokens,
