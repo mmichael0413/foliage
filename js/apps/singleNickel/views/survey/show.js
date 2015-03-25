5cb09ad97a76e71af33dfb8d28c3c367d2b9f19b
Scott Pullen <s.pullen05@gmail.com>
2015-03-25 15:45:16 -0400

    removing initialize from Show view since the model gets set implicitly

--- js/apps/singleNickel/views/survey/show.js
+++ js/apps/singleNickel/views/survey/show.js
@@ -6,9 +6,6 @@ define(function(require) {
 
     return Backbone.View.extend({
         template: HandlebarsTemplates['singleNickel/survey/show'],
-        initialize: function(args) {
-            this.model = args.model;
-        },
         render: function() {
             this.$el.html(this.template(this.model));
             this.$('select').chosen({disable_search: true, width: "100%"});
