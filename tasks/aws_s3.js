1c67b93f2c1b592dedff973ed34522069c1a4040
Andrew Croswell <andrew@thirdchannel.com>
2015-04-02 14:02:26 -0400

    Updated fonts library with new icons

--- tasks/aws_s3.js
+++ tasks/aws_s3.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
                 mime: {
                     'dist/thirdchannel/fonts/nexa_bold-webfont.woff': 'application/font-woff',
                     'dist/thirdchannel/fonts/nexa_light-webfont.woff': 'application/font-woff',
-                    'dist/thirdchannel/fonts/tc-icons_6e130a4e526e6c444098f7055986eb13.woff': 'application/font-woff'
+                    'dist/thirdchannel/fonts/tc-icons_382277c86c3cb9fc70127c36838f5043.woff': 'application/font-woff'
                 }
             },
             files: [
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
                 mime: {
                     'dist/thirdchannel/fonts/nexa_bold-webfont.woff': 'application/font-woff',
                     'dist/thirdchannel/fonts/nexa_light-webfont.woff': 'application/font-woff',
-                    'dist/thirdchannel/fonts/tc-icons_6e130a4e526e6c444098f7055986eb13.woff': 'application/font-woff'
+                    'dist/thirdchannel/fonts/tc-icons_382277c86c3cb9fc70127c36838f5043.woff': 'application/font-woff'
                 }
             },
             files: [
