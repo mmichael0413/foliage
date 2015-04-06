1c180d4af2bf2495318bae1179bdcd37a9a002a6
Scott Pullen <s.pullen05@gmail.com>
2015-04-06 11:10:24 -0400

    TC-1401 - adding heatmap legend with outline around the legend color guide (so you can see the white)

--- js/apps/thirdchannel/templates/hbs-compiled.js
+++ js/apps/thirdchannel/templates/hbs-compiled.js
@@ -1726,7 +1726,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n            <li><i class=\"fa fa-circle\" style=\"color: "
+  buffer += "\n            <li><i class=\"fa fa-square\" style=\"color: "
     + escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0))
     + "\"></i> "
     + escapeExpression(((stack1 = (data == null || data === false ? data : data.key)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
@@ -1740,7 +1740,7 @@ function program1(depth0,data) {
   if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "</p>\n    <ul class=\"legend\">\n        ";
+  buffer += "</p>\n    <ul class=\"heatmap-legend\">\n        ";
   stack1 = helpers.each.call(depth0, ((stack1 = (depth0 && depth0.config)),stack1 == null || stack1 === false ? stack1 : stack1.legend), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n    </ul>\n    <div class=\"heatmap\"><svg></svg></div>\n</div>";
