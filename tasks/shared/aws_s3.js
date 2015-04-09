e3aa478833f90f256682617d1bb1bddd2fe2dddc
Andrew Croswell <andrew@thirdchannel.com>
2015-04-09 07:49:40 -0400

    Removed debug flag in shared aws uploader

--- tasks/shared/aws_s3.js
+++ tasks/shared/aws_s3.js
@@ -19,8 +19,7 @@ module.exports = function(grunt) {
                         'dist/shared/fonts/nexa_bold-webfont.woff': 'application/font-woff',
                         'dist/shared/fonts/nexa_light-webfont.woff': 'application/font-woff',
                         'dist/shared/fonts/tc-icons_c8b56c84192912040a103bcfbd9a7a98.woff': 'application/font-woff'
-                    },
-                    debug: true
+                    }
                 },
                 files: [
                     {dest: 'dist/shared/', cwd: 'backup/staging/shared/', action: 'download'},
