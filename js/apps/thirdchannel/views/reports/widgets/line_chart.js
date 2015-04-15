525d02e3f6ff0c80cfefe64d86b9fe0faa0d4f12
Andrew Croswell <andrew@thirdchannel.com>
2015-04-15 11:16:26 -0400

    Created a rename task which takes the css files of our required libraries and changes them to scss, so that they can be imported in the main scss for each app.

--- js/apps/thirdchannel/views/reports/widgets/line_chart.js
+++ js/apps/thirdchannel/views/reports/widgets/line_chart.js
@@ -2,7 +2,7 @@ define(function(require) {
     var Backbone = require('backbone'),
         Handlebars = require('handlebars'),
         HandlebarsTemplates = require('handlebarsTemplates'),
-        Charts = require('chartjs'),
+        c3 = require('c3'),
         context = require('context');
 
     var defaultLegendColors = ["#F15F51", "#9FB2C0", "#A9BC4D", "#8079b8", "#85c194", "#deb99a", "#bce4f9", "#f69d6d", "#8ab2ca", "#a53426", "#8c8d8e", "#00a55a", "#deb99a", "#ef6222", "#4cc3f1", "#025832", "#585E60"];
@@ -27,54 +27,82 @@ define(function(require) {
         },
 
         setupChart: function () {
-            var self = this,
-                canvas = this.$el.find('canvas');
 
-            var scaleLabel = "value";
-            if(this.model.config.y_prefix !== undefined) {
-                scaleLabel = "'" + this.model.config.y_prefix + "' + " + scaleLabel;
-            }
-            if(this.model.config.y_postfix !== undefined) {
-                scaleLabel = scaleLabel + " + '" + this.model.config.y_postfix + "'";
-            }
-            scaleLabel = "<%= " + scaleLabel + " %>";
 
-            var options = _.extend({
-                animation: false,
-                responsive: true,
-                scaleShowGridLines : true,
-                scaleGridLineColor : "rgba(0,0,0,.05)",
-                scaleGridLineWidth : 1,
-                scaleShowHorizontalLines: true,
-                scaleShowVerticalLines: true,
-                scaleSteps: 10,
-                scaleStepWidth: 10,
-                bezierCurve : true,
-                bezierCurveTension : 0.4,
-                pointDot : true,
-                pointDotRadius : 4,
-                pointDotStrokeWidth : 1,
-                pointHitDetectionRadius : 20,
-                datasetStroke : true,
-                datasetStrokeWidth : 2,
-                datasetFill : false,
-                showTooltips: false,
-                scaleLabel: scaleLabel,
-                defaultLegendColors: this.legendColors,
-                legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-            }, this.model.config);
+            var self = this;
+
+
+//            var self = this,
+//                canvas = this.$el.find('canvas');
+//
+//            var scaleLabel = "value";
+//            if(this.model.config.y_prefix !== undefined) {
+//                scaleLabel = "'" + this.model.config.y_prefix + "' + " + scaleLabel;
+//            }
+//            if(this.model.config.y_postfix !== undefined) {
+//                scaleLabel = scaleLabel + " + '" + this.model.config.y_postfix + "'";
+//            }
+//            scaleLabel = "<%= " + scaleLabel + " %>";
+//
+//            var options = _.extend({
+//                animation: false,
+//                responsive: true,
+//                scaleShowGridLines : true,
+//                scaleGridLineColor : "rgba(0,0,0,.05)",
+//                scaleGridLineWidth : 1,
+//                scaleShowHorizontalLines: true,
+//                scaleShowVerticalLines: true,
+//                scaleSteps: 10,
+//                scaleStepWidth: 10,
+//                bezierCurve : true,
+//                bezierCurveTension : 0.4,
+//                pointDot : true,
+//                pointDotRadius : 4,
+//                pointDotStrokeWidth : 1,
+//                pointHitDetectionRadius : 20,
+//                datasetStroke : true,
+//                datasetStrokeWidth : 2,
+//                datasetFill : false,
+//                showTooltips: false,
+//                scaleLabel: scaleLabel,
+//                defaultLegendColors: this.legendColors,
+//                legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+//            }, this.model.config);
+
+//            if(window.pdf === undefined) {
+//                this.listenTo(context, 'report post render', _.debounce(function () {
+//                    setTimeout(function() {
+//                        new Chart(canvas[0].getContext("2d")).Line(self.model.results, options);
+//                    }, 500);
+//                }, 500));
+//            } else {
+//                this.listenTo(context, 'report post render', function() {
+//                    new Chart(canvas[0].getContext("2d")).Line(self.model.results, options);
+//                });
+//            }
 
             if(window.pdf === undefined) {
                 this.listenTo(context, 'report post render', _.debounce(function () {
                     setTimeout(function() {
-                        new Chart(canvas[0].getContext("2d")).Line(self.model.results, options);
+                        var chart = c3.generate({
+                            bindto: self.$el.find('.chart.line-chart')[0],
+                            data: {
+                                columns: [
+                                    ['data1', 30, 200, 100, 400, 150, 250],
+                                    ['data2', 50, 20, 10, 40, 15, 25]
+                                ]
+                            }
+                        });
                     }, 500);
                 }, 500));
             } else {
                 this.listenTo(context, 'report post render', function() {
-                    new Chart(canvas[0].getContext("2d")).Line(self.model.results, options);
+
                 });
             }
+
+
+
         },
 
         toggleSeries: function(e) {
