5f9aa394a96bf2fa0449c295e17f32751decaaab
Andrew Croswell <andrew@thirdchannel.com>
2015-04-16 15:17:52 -0400

    Fixed procrastination handlebars build issue.

--- tasks/procrastination/handlebars.js
+++ tasks/procrastination/handlebars.js
@@ -9,21 +9,19 @@
 module.exports = function(grunt) {
     return {
         procrastination: {
-            compile: {
-                options: {
-                    // so we can use it with require!
-                    amd: true,
-                    namespace: "procrastination.templates",
-                    processName: function(filename) {
-                        return filename.replace('templates/handlebars/', '').replace('.hbs', '');
-                    }
-                },
-                files: {
-                    "js/apps/procrastination/templates/hbs-compiled.js": [
-                        "templates/handlebars/shared/**/*.hbs",
-                        "templates/handlebars/procrastination/**/*.hbs"
-                    ]
+            options: {
+                // so we can use it with require!
+                amd: true,
+                namespace: "procrastination.templates",
+                processName: function(filename) {
+                    return filename.replace('templates/handlebars/', '').replace('.hbs', '');
                 }
+            },
+            files: {
+                "js/apps/procrastination/templates/hbs-compiled.js": [
+                    "templates/handlebars/shared/**/*.hbs",
+                    "templates/handlebars/procrastination/**/*.hbs"
+                ]
             }
         }
     };
