a44beed72c74397621b48242f3d890ef5f202d08
Scott Pullen <s.pullen05@gmail.com>
2015-06-12 11:07:12 -0400

    TC-1560 - adding validations for separate shipping address

--- js/apps/thirdchannel/views/legal/shipping.js
+++ js/apps/thirdchannel/views/legal/shipping.js
@@ -5,7 +5,6 @@ define(function (require) {
     return {
         init: function () {
             $('.user-form').validate({
-                ignore: [],
                 errorPlacement: function (error, element) {
                     if (element.is(":radio")) {
                         error.appendTo(element.parent().parent().find('p'));
@@ -19,12 +18,35 @@ define(function (require) {
                     'user[address_attributes][state]': {
                         required: true
                     },
+                    'user[shipping_address_attributes][street_1]': {
+                        required: '#different_shipping_address:checked'
+                    },
+                    'user[shipping_address_attributes][city]': {
+                        required: '#different_shipping_address:checked'
+                    },
+                    'user[shipping_address_attributes][state]': {
+                        required: '#different_shipping_address:checked'
+                    },
+                    'user[shipping_address_attributes][zip]': {
+                        required: '#different_shipping_address:checked'
+                    },
                     'user[shirt_size]': {
                         required: true
                     },
                     'user[shoe_size]': {
                         required: true
                     }
+                }
+            });
+
+            var $shippingAddressFields = $('#shipping-address-fields');
+
+            $('#different_shipping_address').on('click', function(e) {
+                if($(this).is(':checked')) {
+                    $shippingAddressFields.show();
+                } else {
+                    $shippingAddressFields.hide();
+                    $shippingAddressFields.find(':input').val('');
                 }
             });
         }
