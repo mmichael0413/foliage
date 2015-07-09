b4d73a715040e550e34fd1dc9afa568549ae5177
Scott Pullen <s.pullen05@gmail.com>
2015-07-09 10:04:22 -0400

    TC-1702 - Using the title (Month Year) instead of the range

--- js/apps/procrastination/templates/hbs-compiled.js
+++ js/apps/procrastination/templates/hbs-compiled.js
@@ -220,7 +220,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<div class=\"pagination-holder\"></div>\n<section class=\"section data-section\">\n    <div class=\"pure-g\">\n        <h3 class=\"header col-1-2\">Range</h3>\n        <h3 class=\"header col-1-2\">Completion Date</h3>\n    </div>\n    <div class=\"body\"></div>\n</section>";
+  return "<div class=\"pagination-holder\"></div>\n<section class=\"section data-section\">\n    <div class=\"pure-g\">\n        <h3 class=\"header col-1-3\">Schedule</h3>\n        <h3 class=\"header col-1-3\">Range</h3>\n        <h3 class=\"header col-1-3\">Completion Date</h3>\n    </div>\n    <div class=\"body\"></div>\n</section>";
   });
 
 this["procrastination"]["templates"]["procrastination/admin/scheduling_cycles_item"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
@@ -229,13 +229,17 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "<p class=\"item col-1-2\"><a href=\""
+  buffer += "<p class=\"item col-1-3\"><a href=\""
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.links)),stack1 == null || stack1 === false ? stack1 : stack1.self)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "\">";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</a></p>\n<p class=\"item col-1-3\">";
   if (helper = helpers.dateRange) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.dateRange); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</a></p>\n<p class=\"item col-1-2\">";
+    + "</p>\n<p class=\"item col-1-3\">";
   if (helper = helpers.scheduleCompletionDate) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.scheduleCompletionDate); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
