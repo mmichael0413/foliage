3aa1ad30b7f4707359e1a8a65c358070d09e0259
Scott Pullen <s.pullen05@gmail.com>
2015-07-09 13:42:10 -0400

    TC-1702 - moving the status percentages population to the model

--- js/apps/procrastination/views/admin/scheduling_cycles_item.js
+++ js/apps/procrastination/views/admin/scheduling_cycles_item.js
@@ -12,24 +12,6 @@ define(function (require) {
             render: function() {
                 var data = this.model.attributes;
 
-                if(data.statusCounts['Complete'] !== undefined) {
-                    data.percentComplete = Math.round((data.statusCounts['Complete'] / data.scheduleCount) * 100.0);
-                } else {
-                    data.percentComplete = 0;
-                }
-
-                if(data.statusCounts['In Progress'] !== undefined) {
-                    data.percentInProgress = Math.round((data.statusCounts['In Progress'] / data.scheduleCount) * 100.0);
-                } else {
-                    data.percentInProgress = 0;
-                }
-
-                if(data.statusCounts['Not Started'] !== undefined) {
-                    data.percentNotStarted = Math.round((data.statusCounts['Not Started'] / data.scheduleCount) * 100.0);
-                } else {
-                    data.percentNotStarted = 0;
-                }
-
                 data.links.self = context.base_url + data.links.self;
 
                 this.$el.html(this.template(data));
