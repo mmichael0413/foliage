858d845f39068e8fe1158a939c0c6ebf62b815e8
Scott Pullen <s.pullen05@gmail.com>
2015-07-29 13:11:03 -0400

    Cleaning up the store service program store list view, moving everything to a module

--- js/apps/stores/routers/main.js
+++ js/apps/stores/routers/main.js
@@ -5,12 +5,9 @@ define(function(require){
         context = require('context'),
         namespacer = require('shared/utils/namespacer'),
         MainLayout = require('shared/views/layout/main'),
-        Actions = require('stores/collections/actions'),
-        ActionsView = require('stores/views/actions'),
         ProgramListView = require('stores/views/programs/list'),
         ProgramStores = require('stores/collections/program_stores'),
-        ProgramStoreListView = require('stores/views/program_stores/list'),
-        FilterModule = require('thirdchannel/views/filter/main');
+        ProgramStoresModule = require('stores/views/program_stores/list_main');
 
     var AppRouter = require('shared/routers/contextAwareBaseRouter').extend({
         container: $('#application'),
@@ -26,41 +23,13 @@ define(function(require){
             this.swap(view);
         },
 
-
         storeList: function(programId) {
-            // TODO: might be better to make this a module (module view?) that manages the actions/filters/list views (that has a leave method to work with swap)
-
-            // TODO: (Some notes) filters/actions views should be more composable
-            //       For instance filter view should just take the collection and filter it directly instead of sending
-            //       a filter event over context and any view should update when the collection receives a reset event
-            //       It might also make more sense to build the filter collection outside of the FilterModule
-
-            var actions = new Actions([
-                {
-                    type: 'link',
-                    link: '/programs/' + programId + '/exports',
-                    className: 'primary',
-                    icon: 'ic_download',
-                    text: 'Export Stores',
-                    bypass: true
-                },
-                {
-                    type: 'button',
-                    className: 'default toggle-filter',
-                    icon: 'ic_filter'
-                }
-            ]);
-            var actionView = new ActionsView({collection: actions});
-            actionView.render();
-
-            window.filterBootstrap.filters_url = '/api/programs/' + programId + '/filters/program_stores';
-            FilterModule.init();
-
             var program = context.programs.get(programId);
             var programStores = new ProgramStores([], {program: program});
 
-            var view = new ProgramStoreListView({model: program, collection: programStores});
-            this.swap(view);
+            var programStoreModule = new ProgramStoresModule({program: program, programStores: programStores});
+
+            this.swap(programStoreModule);
 
             //context.trigger('filter:toggle'); // and this doesn't work...
         },
---
+++ js/apps/stores/views/program_stores/list_main.js
@@ -0,0 +1,47 @@
+define(function(require) {
+    var Backbone = require('backbone'),
+        context = require('context'),
+        Actions = require('stores/collections/actions'),
+        ActionsView = require('stores/views/actions'),
+        ProgramStoreListView = require('stores/views/program_stores/list'),
+        FilterModule = require('thirdchannel/views/filter/main');
+
+    var Main = Backbone.Model.extend({
+        initialize: function() {
+            this.actions = new Actions([
+                {
+                    type: 'link',
+                    link: '/programs/' + this.get('program').id + '/exports',
+                    className: 'primary',
+                    icon: 'ic_download',
+                    text: 'Export Stores',
+                    bypass: true
+                },
+                {
+                    type: 'button',
+                    className: 'default toggle-filter',
+                    icon: 'ic_filter'
+                }
+            ]);
+
+            this.actionsView = new ActionsView({collection: this.actions});
+            this.programStoresListView = new ProgramStoreListView({model: this.get('program'), collection: this.get('programStores')});
+        },
+        render: function() {
+            // TODO: (Some notes) filters/actions views should be more composable
+            //       For instance filter view should just take the collection and filter it directly instead of sending
+            //       a filter event over context and any view should update when the collection receives a reset event
+            //       It might also make more sense to build the filter collection outside of the FilterModule
+            window.filterBootstrap.filters_url = '/api/programs/' + this.get('program').id + '/filters/program_stores';
+            FilterModule.init();
+            this.actionsView.render();
+            return this.programStoresListView.render();
+        },
+        leave: function() {
+            this.actionsView.remove();
+            this.programStoresListView.leave();
+        }
+    });
+
+    return Main;
+});
