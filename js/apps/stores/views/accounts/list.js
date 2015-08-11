ab101d9f72fed08a6783ba707577527b73d71fe9
Scott Pullen <s.pullen05@gmail.com>
2015-08-11 15:34:02 -0400

    TC-1830 - adding what to do if the an account is added or removed

--- js/apps/stores/views/accounts/list.js
+++ js/apps/stores/views/accounts/list.js
@@ -9,6 +9,8 @@ define(function(require) {
         childViews: [],
         initialize: function() {
             _.bindAll(this, 'renderAccountItem');
+            this.listenTo(this.collection, 'add', this.renderAccountItem);
+            this.listenTo(this.collection, 'remove', this.removeAccountItem);
         },
         render: function() {
             this.$el.html(this.template());
@@ -19,6 +21,20 @@ define(function(require) {
             var v = new ListItemView({model: account});
             this.$('#account-list').append(v.render().el);
             this.childViews.push(v);
+        },
+        removeAccountItem: function(account) {
+            // find and remove the child view
+            var view = _.find(this.childViews, function(v) {
+                return v.model.id === account.id;
+            });
+
+            if(view) {
+                var index = _.indexOf(this.childViews, view);
+            }
+
+            if(index && index >= 0) {
+                this.childViews.splice(index, 1);
+            }
         },
         leave: function() {
             _.each(this.childViews, function(v) {
