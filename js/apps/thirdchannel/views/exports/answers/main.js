ad014ae90987b6556ec2df05e99ee2eb6417def9
Scott Pullen <s.pullen05@gmail.com>
2015-09-14 15:56:08 -0400

    TC-1961 - adding view to handle answers export view

---
+++ js/apps/thirdchannel/views/exports/answers/main.js
@@ -0,0 +1,20 @@
+define(function(require) {
+    var Backbone = require('backbone'),
+        Handlebars = require('handlebars'),
+        HandlebarsTemplates = require('handlebarsTemplates'),
+        DateTimePicker = require('dateTimePicker'),
+        context = require('context');
+
+    return Backbone.View.extend({
+        el: '#answers-export-form',
+
+        render: function() {
+            this.$('#start_date, #end_date').datetimepicker({
+                timepicker:false,
+                format:'m/d/Y'
+            });
+
+            return this;
+        }
+    });
+});
