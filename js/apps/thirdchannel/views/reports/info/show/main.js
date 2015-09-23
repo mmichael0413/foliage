a75e69ba3b539e91972ade020b762eeea9458644
Scott Pullen <s.pullen05@gmail.com>
2015-09-23 10:00:51 -0400

    TC-1961 - updating the view breakdown export

--- js/apps/thirdchannel/views/reports/info/show/main.js
+++ js/apps/thirdchannel/views/reports/info/show/main.js
@@ -1,12 +1,15 @@
 define(function(require) {
     var $ = require('jquery'),
         ExportView = require('thirdchannel/views/utils/csv_export_button'),
+        ExportModel = require('thirdchannel/models/reports/export'),
         ReportInfoView = require('thirdchannel/views/reports/info/show/info_list');
 
     return {
         init: function (options) {
-            $(".actions .export").each(function(){
-                new ExportView(options).render(this);
+            $(".actions .export").each(function() {
+                var data = {baseUrl: '/programs/' + options.programId + "/reports/" + options.reportId + "/info/" + options.infoId + "/export"};
+                var model = new ExportModel(_.extend(data, options));
+                new ExportView({model: model}).render(this);
             });
             new ReportInfoView(options).render();
         }
