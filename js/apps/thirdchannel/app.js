0b90107f25fb71ae36e32c7df638a610a9d7ccb2
Andrew Croswell <andrew@thirdchannel.com>
2015-11-13 13:24:38 -0500

    Forgot to remove the reference to the application router file

--- js/apps/thirdchannel/app.js
+++ js/apps/thirdchannel/app.js
@@ -3,8 +3,7 @@ define(function(require){
     var $ = require('jquery'),
         _ = require('underscore'),
         Backbone = require('backbone'),
-        Router = require('thirdchannel/routers/router'),
-        ApplicationRouter = require('thirdchannel/routers/application_router');
+        Router = require('thirdchannel/routers/router');
 
         var initialize = function(){
             Router.initialize();
