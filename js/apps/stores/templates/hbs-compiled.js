4a96c7add93c2f7943fd2ed9414ee39d9887c3a2
Scott Pullen <s.pullen05@gmail.com>
2015-11-19 09:01:26 -0500

    TC-2418 - adding a message to say that everything was valid on the review issues page

--- js/apps/stores/templates/hbs-compiled.js
+++ js/apps/stores/templates/hbs-compiled.js
@@ -1366,14 +1366,21 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 this["Stores"]["templates"]["stores/uploads/show"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  
+  return "\n    <h2>No invalid records</h2>\n";
+  }
 
   buffer += "<div>\n    <a href=\"/programs/";
   if (helper = helpers.programId) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.programId); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
     + "/uploads\" class=\"btn primary\"><i class=\"ic fa ic_left\"></i> Back to upload history</a>\n</div>\n<br>\n";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.valid), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer;
   });
 
