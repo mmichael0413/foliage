2000f1a41365a3baa0d6cd9b813c58cb79ab5974
Scott Pullen <s.pullen05@gmail.com>
2015-12-09 08:38:27 -0500

    TC-2045 - using the base export view

--- js/apps/thirdchannel/views/exports/sales_stores/main.js
+++ js/apps/thirdchannel/views/exports/sales_stores/main.js
@@ -5,38 +5,15 @@ define(function(require) {
         HandlebarsTemplates = require('handlebarsTemplates'),
         Serialize = require('serializeObject'),
         context = require('context'),
+        BaseExportView = require('thirdchannel/views/exports/base'),
         ExportModel = require('thirdchannel/models/exports/sales_stores'),
         ExportModal = require('thirdchannel/modals/export');
 
-    return Backbone.View.extend({
+    return BaseExportView.extend({
         el: '#sales-stores-export-form',
 
-        events: {
-            'submit': 'initiateExport'
-        },
-
-        render: function() {
-            return this;
-        },
-
-        initiateExport: function(e) {
-            e.preventDefault();
-            this.$('.error').removeClass('error');
-            var model = new ExportModel(_.extend(this.$el.serializeObject(), {programId: context.programId}));
-            model.save().then(function() {
-                var modal = new ExportModal({model: model});
-                $("body").append(modal.render().el);
-            }).fail(function(response) {
-                if(response.status === 422) {
-                    var error = response.responseJSON;
-                    alert(error.error);
-                    _.each(error.errors, function(v, k) {
-                        this.$('#' + k).addClass('error');
-                    }.bind(this));
-                } else {
-                    alert('Something went wrong.');
-                }
-            }.bind(this));
+        initialize: function() {
+            this.model = new ExportModel();
         }
     });
 });
