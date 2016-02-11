61ac05ca2a05d84c503ff88d562f3bb47b615856
Scott Pullen <s.pullen05@gmail.com>
2016-02-11 11:01:12 -0500

    TC-2898 - displaying the details link for all sales data scenarios

--- js/apps/thirdchannel/templates/hbs-compiled.js
+++ js/apps/thirdchannel/templates/hbs-compiled.js
@@ -1137,16 +1137,16 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 this["ThirdChannel"]["templates"]["thirdchannel/activities/sales_widget"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
 
 function program1(depth0,data) {
   
   var buffer = "", stack1, helper;
-  buffer += "\n    <p class=\"text full\">";
+  buffer += "\n    <p class=\"text\">";
   if (helper = helpers.message) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.message); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "</p>\n    ";
+    + "</p>\n  ";
   return buffer;
   }
 
@@ -1162,11 +1162,7 @@ function program3(depth0,data) {
     + "</span> ";
   stack1 = helpers['if'].call(depth0, (depth0 && depth0.showLabel), {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "</p>\n    <a class=\"details\" href=\"";
-  if (helper = helpers.salesUrl) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.salesUrl); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "\">DETAILS</a>\n    ";
+  buffer += "</p>\n  ";
   return buffer;
   }
 function program4(depth0,data) {
@@ -1175,10 +1171,14 @@ function program4(depth0,data) {
   return " From LY";
   }
 
-  buffer += "<section class=\"sales-widget\">\n    ";
+  buffer += "<section class=\"sales-widget\">\n  ";
   stack1 = helpers['if'].call(depth0, (depth0 && depth0.message), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n    \n</section>    ";
+  buffer += "\n  <a class=\"details\" href=\"";
+  if (helper = helpers.salesUrl) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.salesUrl); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "\">DETAILS</a>\n</section>    ";
   return buffer;
   });
 
