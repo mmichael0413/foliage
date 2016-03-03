5dedec83e670bb0f1e4cca43b2fdc314cc2fe76a
eliottwiener-thirdchannel <eliott@thirdchannel.com>
2016-03-03 15:29:55 -0500

    tc-2610: simplify loop

--- js/apps/thirdchannel/views/checkins/job_list.js
+++ js/apps/thirdchannel/views/checkins/job_list.js
@@ -9,15 +9,13 @@ define(function(require){
         loadingHTML: HandlebarsTemplates['thirdchannel/loading_icon'],
         render: function() {
             _.chain(this.model.job_uuids_by_date).keys().sort().each(function(date){
-                _.each(this.model.job_uuids_by_date, function(job_uuids, date){
-                    _.each(job_uuids, function(job_uuid){
-                        var jobView = new JobView({model: {
-                            job: this.model.job_details_by_uuid[job_uuid],
-                            date: date,
-                            store: this.model.store_details
-                        }});
-                        this.$el.append(jobView.render().el);
-                    }.bind(this));
+                _.each(this.model.job_uuids_by_date[date], function(job_uuid){
+                    var jobView = new JobView({model: {
+                        job: this.model.job_details_by_uuid[job_uuid],
+                        date: date,
+                        store: this.model.store_details
+                    }});
+                    this.$el.append(jobView.render().el);
                 }.bind(this));
             }.bind(this));
             return this;
