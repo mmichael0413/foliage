0939628af970842d6f57f7737d2206e7d6421b3b
Andrew Croswell <andrew@thirdchannel.com>
2016-03-11 10:36:49 -0500

    Updated to call the list async.

--- js/apps/erudition/views/recruiting/users.js
+++ js/apps/erudition/views/recruiting/users.js
@@ -1,19 +1,24 @@
 define(function (require) {
     var Backbone = require('backbone'),
         context = require('context'),
-        templates = require('handlebarsTemplates');
+        templates = require('handlebarsTemplates'),
+        Users = require('erudition/collections/users');
 
     return Backbone.View.extend({
         el: '.content',
 
-        viewTemplate: templates['erudition/recruiting/users/list'],
+        viewTemplate: templates['erudition/recruiting/users/index'],
+        userListTemplate: templates['erudition/recruiting/users/users_list'],
 
         initialize: function () {
-            this.collection = context;
+            this.collection = new Users();
         },
 
         render: function () {
-            this.$el.append(this.viewTemplate(this.collection));
+            this.$el.append(this.viewTemplate());
+            this.collection.fetch({success: function (collection) {
+                this.$('#users-list').html(this.userListTemplate(collection.toJSON()));
+            }.bind(this)});
             return this;
         }
     });
--- js/apps/thirdchannel/templates/hbs-compiled.js
+++ js/apps/thirdchannel/templates/hbs-compiled.js
@@ -3,6 +3,15 @@ define(['handlebars'], function(Handlebars) {
 this["ThirdChannel"] = this["ThirdChannel"] || {};
 this["ThirdChannel"]["templates"] = this["ThirdChannel"]["templates"] || {};
 
+Handlebars.registerPartial("loading", Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<div class=\"section loading-section\">\n    <div class=\"loading\">\n        <div class=\"fa fa-spin ic_processing\"></div> Loading...\n    </div>\n</div>\n\n";
+  }));
+
 Handlebars.registerPartial("content", Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
@@ -4526,7 +4535,20 @@ function program26(depth0,data) {
   return buffer;
   });
 
-this["ThirdChannel"]["templates"]["erudition/recruiting/users/list"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+this["ThirdChannel"]["templates"]["erudition/recruiting/users/index"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partials, Handlebars.partials); data = data || {};
+  var buffer = "", stack1, self=this;
+
+
+  buffer += "<h1>Users</h1>\n<div id=\"users-list\">\n    ";
+  stack1 = self.invokePartial(partials.loading, 'loading', depth0, helpers, partials, data);
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</div>";
+  return buffer;
+  });
+
+this["ThirdChannel"]["templates"]["erudition/recruiting/users/users_list"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partials, Handlebars.partials); data = data || {};
   var buffer = "", stack1, self=this;
@@ -4550,10 +4572,10 @@ function program2(depth0,data) {
   return buffer;
   }
 
-  buffer += "<h1>Users</h1>\n<div id=\"users-list\" class=\"card\">\n    <div class=\"header pure-g\">\n        <div class=\"col-2-5 col-md-1\">Agent</div>\n        <div class=\"hidden-md col-2-5\">Email</div>\n        <div class=\"hidden-md col-1-5\">Actions</div>\n    </div>\n\n    ";
+  buffer += "<div class=\"card\">\n    <div class=\"header pure-g\">\n        <div class=\"col-2-5 col-md-1\">Agent</div>\n        <div class=\"hidden-md col-2-5\">Email</div>\n        <div class=\"hidden-md col-1-5\">Actions</div>\n    </div>\n\n    ";
   stack1 = helpers.each.call(depth0, depth0, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n</div>";
+  buffer += "\n</div>\n";
   return buffer;
   });
 
