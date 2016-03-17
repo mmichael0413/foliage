e4e2416e29c7f5c6c20bebce01efb6d24c70b377
eliottwiener-thirdchannel <eliott@thirdchannel.com>
2016-03-17 10:35:08 -0400

    tc-2749: don't need the schedule test

--- js/apps/thirdchannel/templates/hbs-compiled.js
+++ js/apps/thirdchannel/templates/hbs-compiled.js
@@ -4086,37 +4086,6 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   return buffer;
   });
 
-this["ThirdChannel"]["templates"]["thirdchannel/store_profile/schedule/list"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
-
-function program1(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\n      <tr>\n        <td>"
-    + escapeExpression(((stack1 = (depth0 && depth0.month)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</td>\n        <td><input type=\"number\" name=\""
-    + escapeExpression(((stack1 = (depth0 && depth0.month)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "_core\" class=\"additionalVisit no-margin\" value=\""
-    + escapeExpression(((stack1 = (depth0 && depth0.occurrences)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\"/></td>\n        <td><input type=\"number\" name=\""
-    + escapeExpression(((stack1 = (depth0 && depth0.month)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "_additional\" class=\"additionalVisit no-margin\" value=\""
-    + escapeExpression(((stack1 = (depth0 && depth0.additional_occurrences)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\"/></td>\n        <td>"
-    + escapeExpression(((stack1 = (depth0 && depth0.total_occurrences)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</td>\n      </tr>\n      ";
-  return buffer;
-  }
-
-  buffer += "<form id=\"scheduleForm\">\n  <table>\n    <thead>\n      <tr>\n        <th>Month</th>\n        <th>Core Visits</th>\n        <th>Additional Visits</th>\n        <th>Total Visits</th>\n      </tr>\n    </thead>\n    <tbody>\n      ";
-  stack1 = helpers.each.call(depth0, (depth0 && depth0.schedule), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n    </tbody>\n  </table>\n  <button class=\"subitSchedule btn primary solid\">Update Schedule</button>\n</form>";
-  return buffer;
-  });
-
 this["ThirdChannel"]["templates"]["thirdchannel/stores/rows"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
