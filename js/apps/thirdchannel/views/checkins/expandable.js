a8bec637dc0050cf1760236824c867c8fe8ae133
eliottwiener-thirdchannel <eliott@thirdchannel.com>
2016-03-23 12:49:51 -0400

    tc-3272: preventDefault to avoid filter weirdness

--- js/apps/thirdchannel/views/checkins/expandable.js
+++ js/apps/thirdchannel/views/checkins/expandable.js
@@ -28,6 +28,7 @@ define(function(require){
             return this;
         },
         opensubsection: function(e){
+            e.preventDefault();
             this.toggle.removeClass('open');
             this.toggle.html(this.closeText + "&nbsp;<i class='ic ic_up'></i>");
             this.toggle.addClass('close');
@@ -43,6 +44,7 @@ define(function(require){
             }
         },
         closesubsection: function(e){
+            e.preventDefault();
             this.toggle.removeClass('close');
             this.toggle.html(this.openText + "&nbsp;<i class='ic ic_down'></i>");
             this.toggle.addClass('open');
