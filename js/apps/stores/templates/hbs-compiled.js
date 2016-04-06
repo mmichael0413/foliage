de941b59e3c5d8826cc0dce9fcf77a3df2a00a42
Scott Pullen <s.pullen05@gmail.com>
2016-04-06 10:20:45 -0400

    hbs-comp

--- js/apps/stores/templates/hbs-compiled.js
+++ js/apps/stores/templates/hbs-compiled.js
@@ -1037,14 +1037,12 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 this["Stores"]["templates"]["stores/program_stores/tag_item"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+  var stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
 
 
   if (helper = helpers.content) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.content); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "\n";
-  return buffer;
+  return escapeExpression(stack1);
   });
 
 this["Stores"]["templates"]["stores/program_stores/tags"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
