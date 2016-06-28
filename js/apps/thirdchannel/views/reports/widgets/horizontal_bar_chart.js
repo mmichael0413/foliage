5a1910088fc915371cbcb435812fee78eb05cca8
Andrew Croswell <andrew@thirdchannel.com>
2016-06-28 07:31:52 -0400

    Fix for TC-4075

--- js/apps/thirdchannel/views/reports/widgets/horizontal_bar_chart.js
+++ js/apps/thirdchannel/views/reports/widgets/horizontal_bar_chart.js
@@ -25,6 +25,7 @@ define(function(require) {
                     this.listenTo(context, 'filter:queryString', function (qs) {
                         this.updateViewBreakDownLink(qs, this.model);
                     });
+                    context.trigger('filter:request:queryString');
                 }
                 if (this.model.uuid) {
                     this.listenTo(context, 'report post render widget_' + this.model.uuid, this.renderChart);
