dc61ccc0f911fcdf3b59cadc36d776933012739c
Steve Pember <spember@gmail.com>
2016-08-17 15:10:29 -0400

    -tweaks to styling based on Marlene's feedback
    -cleaned up a bad console.log
    -resolves tc-4583
    -adds truncation of long fixture names for their type tiles

--- js/apps/thirdchannel/views/fixtures/typeTile.js
+++ js/apps/thirdchannel/views/fixtures/typeTile.js
@@ -22,7 +22,6 @@ define(function(require) {
                 if (data.problems.constructor === Array) {
                     qs += data.problems.join("&problems[]=");
                     data.problemsDisplay = qs;
-                    console.log(data.problemsDisplay);
                 } else {
                     console.warn("Problems needs to be an array before it can be turned into a query string. instead, received a ", typeof data.problems);
                 }
@@ -31,7 +30,8 @@ define(function(require) {
 
             render: function () {
                 var attributes = this.model.get("attributes"),
-                    data = this.model.toJSON();
+                    data = this.model.toJSON(),
+                    nameMax = 45;
                     
                 if (attributes.hasOwnProperty("fixtureImages") && attributes.fixtureImages.length >= 1) {
                     //data.imageUrl = attributes.fixtureImages[attributes.fixtureImages.length-1].links[2].href
@@ -43,6 +43,11 @@ define(function(require) {
                 
                 if (data.problems) {
                     this.buildProblemsQueryString(data);
+                }
+                
+                // tc-4583, truncates the name of the fixture if it's too long
+                if (data.name.length > nameMax) {
+                    data.name = data.name.substring(0,nameMax-1) + "...";
                 }
 
                 this.$el.html(HandlebarsTemplates["thirdchannel/fixtures/fixture_type_tile"](data));
