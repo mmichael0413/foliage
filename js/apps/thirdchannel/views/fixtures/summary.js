27f098062a50b421d7ee3ceb9f60047030a29577
Scott Pullen <s.pullen05@gmail.com>
2016-11-17 08:45:09 -0500

    CQ-315 - attaching the query string to the fixture links

--- js/apps/thirdchannel/views/fixtures/summary.js
+++ js/apps/thirdchannel/views/fixtures/summary.js
@@ -1,5 +1,7 @@
 define(function(require){
-    var Backbone = require('backbone'),
+    var _ = require('underscore'),
+        $ = require('jquery'),
+        Backbone = require('backbone'),
         buttons = require('buttons'),
         context = require('context'),
         HandlebarsTemplates = require('handlebarsTemplates'),
@@ -14,14 +16,26 @@ define(function(require){
 
                 this.listenTo(this.model, 'request', this.renderLoading);
                 this.listenTo(this.model, 'change', this.render);
+                this.listenTo(context, 'filter:query', this.render);
             },
 
             renderLoading: function() {
-                this.$el.html(HandlebarsTemplates["thirdchannel/fixtures/overview_loading"]);
+                this.$el.html(HandlebarsTemplates["thirdchannel/fixtures/summary_overview_loading"]);
             },
 
             render: function() {
-                this.$el.html(this.template(this.model.attributes));
+                var data = this.model.attributes;
+                data.programId = context.programId;
+                data.queryString = window.location.search;
+                data.problemsQueryString = data.queryString;
+
+                var problems = this.model.get('problems');
+                if(problems !== undefined && problems !== null && !_.isEmpty(problems)) {
+                    data.problemsQueryString = data.problemsQueryString + '&' + $.param({problems: problems});
+                }
+
+                this.$el.html(this.template(data));
+
                 return this;
             }
 
