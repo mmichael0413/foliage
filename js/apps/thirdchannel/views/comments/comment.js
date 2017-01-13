f3f439b815e6599cdfbbd6ecceab119e8d75db00
eliottwiener-thirdchannel <eliott@thirdchannel.com>
2017-01-13 13:44:04 -0500

    formatting

--- js/apps/thirdchannel/views/comments/comment.js
+++ js/apps/thirdchannel/views/comments/comment.js
@@ -1,11 +1,11 @@
-define(function(require){
+define(function(require) {
     var Backbone = require('backbone'),
         HandlebarsTemplates = require('handlebarsTemplates');
-        
+
     return Backbone.View.extend({
         className: 'comment',
         template: HandlebarsTemplates['thirdchannel/comment'],
-        initialize: function (options) {
+        initialize: function(options) {
             this.model = options.model;
             this.model.set('mentions', options.mentions);
             this.model.set('currentUserId', options.currentUserId);
@@ -15,11 +15,11 @@ define(function(require){
         events: {
             'click .delete-comment': 'deleteComment'
         },
-        render: function () {
+        render: function() {
             this.$el.html(this.template(this.model.attributes));
             return this;
         },
-        deleteComment: function (e) {
+        deleteComment: function(e) {
             e.preventDefault();
             e.stopPropagation();
 
@@ -27,11 +27,13 @@ define(function(require){
             var deleteUrl = this.model.url + '?comment_id=' + this.model.get('comment_id');
 
             this.model.collection.remove(this.model);
-            this.model.destroy({url: deleteUrl}).done(function () {
+            this.model.destroy({
+                url: deleteUrl
+            }).done(function() {
                 self.remove();
             });
         },
-        removeFromDom: function () {
+        removeFromDom: function() {
             this.remove();
         }
     });
