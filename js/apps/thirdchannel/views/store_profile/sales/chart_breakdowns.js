88aad4542a30b31fabba456a23bbf12e779bb2fc
Chris Stallings <chris@thirdchannel.com>
2017-01-31 09:35:45 -0500

    Created a string util with titleize function

--- js/apps/thirdchannel/views/store_profile/sales/chart_breakdowns.js
+++ js/apps/thirdchannel/views/store_profile/sales/chart_breakdowns.js
@@ -4,19 +4,10 @@ define(function(require) {
         context = require('context'),
         c3 = require('c3'),
         Handlebars = require('handlebars'),
+        string_utils = require('thirdchannel/utils/string_utils'),
         HandlebarsTemplates = require('handlebarsTemplates');
 
     var defaultLegendColors = ["#F15F51", "#585E60", "#9FB2C0", "#A9BC4D"];
-
-    var titleizeString = function(text) {
-        if (text) {
-            return text.split(' ').map(function (s) {
-                return s.length <= 1 ? s.toUpperCase() : s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
-            }).join(" ");
-        } else {
-            return "";
-        }
-    };
 
     var View = Backbone.View.extend({
         template: HandlebarsTemplates['thirdchannel/store_profile/sales/chart_breakdowns'],
@@ -43,7 +34,7 @@ define(function(require) {
 
             breakdowns = _.map(breakdowns, function(data, key) {
                         //Title Case the labels
-                        data.label = titleizeString(key);
+                        data.label = string_utils.titleize(key);
                         data.percentageOfSales = (data.salesInCents / store.salesInCents) * 100;
                         return data;
             });
@@ -134,7 +125,7 @@ define(function(require) {
             }
 
             breakdowns = _.map(breakdowns, function(data, key) {
-                data.label = titleizeString(key);
+                data.label = string_utils.titleize(key);
                 return data;
             });
             breakdowns = _.filter(breakdowns, function(data) { return data.percentageOfSales !== null; });
