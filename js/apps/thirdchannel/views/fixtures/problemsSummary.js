5cd02f45c61073dbfbc3040b0b279f91c01cad8d
Scott Pullen <s.pullen05@gmail.com>
2017-02-02 15:36:14 -0500

    CQ-321 - removing an unused require

--- js/apps/thirdchannel/views/fixtures/problemsSummary.js
+++ js/apps/thirdchannel/views/fixtures/problemsSummary.js
@@ -5,7 +5,6 @@ define(function(require){
         buttons = require('buttons'),
         context = require('context'),
         HandlebarsTemplates = require('handlebarsTemplates'),
-        FilterableComponent = require('shared/views/utils/filterable_component'),
         ProblemItemView = require('thirdchannel/views/fixtures/problems/item'),
 
         Summary = Backbone.View.extend({
