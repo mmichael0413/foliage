60421d6ad2137dab4b4a4460530ba32f3b7f3aa1
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-08 13:34:20 -0500

    base layout for activity reports rollup section now added to current associate education section

--- js/apps/thirdchannel/views/reports/field_activity/report.js
+++ js/apps/thirdchannel/views/reports/field_activity/report.js
@@ -1,12 +1,14 @@
 define(function(require) {
     var Backbone = require('backbone'),
         context = require('context'),
-        OverviewView = require('thirdchannel/views/reports/field_activity/overview');
+        OverviewView = require('thirdchannel/views/reports/field_activity/overview'),
+        AssociateEducationView = require('thirdchannel/views/reports/field_activity/associate_education');
 
     return Backbone.View.extend({
-  
+
         initialize: function() {
           new OverviewView();
+          new AssociateEducationView();
         }
     });
 });
