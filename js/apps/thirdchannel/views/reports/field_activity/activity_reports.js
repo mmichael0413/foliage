16932d3eb0b4c39d0fb9219da033676427813cb8
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-08 15:45:00 -0500

    add what icons we do have to activity reports rollup

--- js/apps/thirdchannel/views/reports/field_activity/activity_reports.js
+++ js/apps/thirdchannel/views/reports/field_activity/activity_reports.js
@@ -1,17 +1,29 @@
 define(function(require) {
     var $ = require('jquery'),
+        _ = require('underscore'),
         Backbone = require('backbone'),
         context = require('context'),
         HandlebarsTemplates = require('handlebarsTemplates');
 
     return Backbone.View.extend({
       template: HandlebarsTemplates['thirdchannel/reports/field_activity/activity_reports'],
+      iconMapping: {
+        "Visits Completed": "ic_visit",
+        "Stores Visited": "ic_store"
+      },
 
       events: {
         'click .activity-reports-toggle': 'toggleActivityReportsList'
       },
 
       initialize: function() {
+        var self = this;
+
+        this.model.rollups = _.map(this.model.rollups, function(rollup) {
+          rollup.icon = self.iconMapping[rollup.label];
+          return rollup;
+        });
+
         this.render();
       },
 
