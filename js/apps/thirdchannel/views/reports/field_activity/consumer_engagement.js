035274612ea0497651c4117a4a07445ce573c92a
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-08 14:10:20 -0500

    create consumer engagement section

--- js/apps/thirdchannel/views/reports/field_activity/consumer_engagement.js
+++ js/apps/thirdchannel/views/reports/field_activity/consumer_engagement.js
@@ -22,11 +22,9 @@ define(function(require) {
       render: function() {
         this.$el.html(this.template(this.model.attributes));
         new ActivityReportsView({model: this.model.get('activityReport'), el: '.consumer-engagement-activity-reports'});
-
         _.each(this.model.get('metrics'), function(metric) {
           new KPIView({model: metric, el: '.consumer-engagement-kpis'});
         });
-
         return this;
       }
     });
