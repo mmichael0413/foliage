99f32ad38db109366b898adc1fcc4b17050637a4
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-09 15:35:28 -0500

    Update icon library with person and states, and swapped them into the page

--- js/apps/thirdchannel/models/reports/field_activity/overview.js
+++ js/apps/thirdchannel/models/reports/field_activity/overview.js
@@ -1,7 +1,16 @@
 define(function(require) {
   var Backbone = require('backbone');
+  var _ = require('underscore');
 
   return Backbone.Model.extend({
+
+    iconMapping: {
+      "Visits Completed": "fa fa-hand-lizard-o",
+      "Stores Visited": "fa fa-hand-lizard-o",
+      "States Visited": "ic ic_states",
+      "ThirdChannel Agents": "ic ic_person agent",
+      "In-Store Support/FMRs": "ic ic_person fmr"
+    },
 
     initialize: function (options) {
       this.options = options;
@@ -11,6 +20,18 @@ define(function(require) {
       // return '/programs/' + this.options.programId + "/reports/" + this.options.surveyType + "/" + this.options.typeId + "/question/" + this.options.questionId + "?format=json";
 
       return '/programs/Merchandising/reports/field_activities';
+    },
+
+    mapIconToMetric: function() {
+      var self = this;
+
+      var model = _.clone(this.get('fieldActivities'));
+      model.metrics = _.map(model.metrics, function(metric) {
+        metric.icon = self.iconMapping[metric.label];
+        return metric;
+      });
+
+      this.set('fieldActivities', model);
     }
 
   });
