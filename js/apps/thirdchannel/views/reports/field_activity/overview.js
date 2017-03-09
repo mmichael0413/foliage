e712abae319a4bd5ee1ba4da4da65f15278753ca
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-09 15:35:28 -0500

    Update icon library with person and states, and swapped them into the page

--- js/apps/thirdchannel/views/reports/field_activity/overview.js
+++ js/apps/thirdchannel/views/reports/field_activity/overview.js
@@ -10,6 +10,8 @@ define(function(require) {
       template: HandlebarsTemplates['thirdchannel/reports/field_activity/overview'],
 
       initialize: function() {
+        var self = this;
+
         this.model = new OverviewModel(/* Probably pass program params */);
 
         this.model.fetch().done(function(response) {
