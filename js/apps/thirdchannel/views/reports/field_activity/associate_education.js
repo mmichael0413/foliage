0b97901bb6cfd31a56a61be54154b743f5285019
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-09 12:34:03 -0500

    All the KPIS and lizard hands

--- js/apps/thirdchannel/views/reports/field_activity/associate_education.js
+++ js/apps/thirdchannel/views/reports/field_activity/associate_education.js
@@ -1,13 +1,15 @@
 define(function(require) {
     var $ = require('jquery'),
+        _ = require('underscore'),
         Backbone = require('backbone'),
         context = require('context'),
         HandlebarsTemplates = require('handlebarsTemplates'),
         AssociateEducationModel = require('thirdchannel/models/reports/field_Activity/associate_education'),
-        ActivityReportsView = require('thirdchannel/views/reports/field_activity/activity_reports');
+        ActivityReportsView = require('thirdchannel/views/reports/field_activity/activity_reports'),
+        KPIView = require('thirdchannel/views/reports/field_activity/KPI');
 
     return Backbone.View.extend({
-      el: ".associate-education",
+      el: '.associate-education',
       template: HandlebarsTemplates['thirdchannel/reports/field_activity/associate_education'],
 
       initialize: function() {
@@ -21,6 +23,11 @@ define(function(require) {
       render: function() {
         this.$el.html(this.template(this.model.attributes));
         new ActivityReportsView({model: this.model.get('activityReport'), el: '.associate-education-activity-reports'});
+
+        _.each(this.model.get('metrics'), function(metric) {
+          new KPIView({model: metric, el: '.associate-education-kpis'});
+        });
+
         return this;
       }
     });
