f0a5014af938fd8e8a7f27f9c42dd5f84ce16c18
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-17 04:58:46 -0400

    more oddly capped file paths

--- js/apps/thirdchannel/views/reports/field_activity/merchandising.js
+++ js/apps/thirdchannel/views/reports/field_activity/merchandising.js
@@ -3,7 +3,7 @@ define(function(require) {
         Backbone = require('backbone'),
         context = require('context'),
         HandlebarsTemplates = require('handlebarsTemplates'),
-        MerchandisingModel = require('thirdchannel/models/reports/field_Activity/merchandising'),
+        MerchandisingModel = require('thirdchannel/models/reports/field_activity/merchandising'),
         ActivityReportsView = require('thirdchannel/views/reports/field_activity/activity_reports'),
         KPIView = require('thirdchannel/views/reports/field_activity/KPI');
 
