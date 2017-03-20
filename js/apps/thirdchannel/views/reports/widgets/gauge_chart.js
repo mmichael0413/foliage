1e518e1debe92a5405818be1e50e2115bfe4f7e4
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-03-20 12:19:21 -0400

    vertical bar chart

--- js/apps/thirdchannel/views/reports/widgets/gauge_chart.js
+++ js/apps/thirdchannel/views/reports/widgets/gauge_chart.js
@@ -17,13 +17,6 @@ define(function(require) {
         render: function () {
             this.setElement(this.template(this.model));
 
-            if (this.model.show_view_list !== undefined) {
-                this.listenTo(context, 'filter:queryString', function (qs) {
-                    this.updateViewBreakDownLink(qs, this.model);
-                });
-                context.trigger('filter:request:queryString');
-            }
-
             if (this.model.uuid) {
                 this.listenTo(context, 'report post render widget_' + this.model.uuid, this.renderChart);
             } else {
