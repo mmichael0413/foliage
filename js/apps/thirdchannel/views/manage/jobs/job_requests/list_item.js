ab6b9a0f52d4bd1619dee89f58a837777aea05aa
Scott Pullen <s.pullen05@gmail.com>
2017-03-24 12:56:36 -0400

    TC-5241 - adding assignee, date scheduled, and date completed to the job requests list

--- js/apps/thirdchannel/views/manage/jobs/job_requests/list_item.js
+++ js/apps/thirdchannel/views/manage/jobs/job_requests/list_item.js
@@ -1,10 +1,14 @@
 define(function(require) {
     var context = require('context'),
-        handlebarsTemplates = require('handlebarsTemplates');
+        HandlebarsTemplates = require('handlebarsTemplates');
+
     return Backbone.View.extend({
-        template: 'thirdchannel/manage/jobs/job_requests/list_item',
-        render: function(){
-            this.$el.html(handlebarsTemplates[this.template](this.model));
+        template: HandlebarsTemplates['thirdchannel/manage/jobs/job_requests/list_item'],
+
+        render: function() {
+            var data = this.model;
+            data.duration = data.duration / 60.0;
+            this.$el.html(this.template(data));
             return this;
         }
     });
