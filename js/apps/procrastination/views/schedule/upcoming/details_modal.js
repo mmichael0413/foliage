8bd8aea301ad8a72b2ebc8ce36b0a6efabcce41b
eliottwiener-thirdchannel <eliott@thirdchannel.com>
2017-03-24 12:29:47 -0400

    tc-5234: style details modal

--- js/apps/procrastination/views/schedule/upcoming/details_modal.js
+++ js/apps/procrastination/views/schedule/upcoming/details_modal.js
@@ -4,13 +4,14 @@ define(function (require) {
         HandlebarsTemplates = require('handlebarsTemplates');
     DetailsModalView = Backbone.View.extend({
         template: HandlebarsTemplates['procrastination/schedule/upcoming/visit_label'],
+        className: 'details-modal',
         render: function () {
-            console.log(this.model);
+            this.model.showOptionalTasks = this.model.jobDetails.programUUID !== '219f1927-7997-4c03-a5c9-4a6b4d792d3e'; // vega
             this.$el.html(this.template(this.model));
             this.$el.dialog({
                 modal: true,
-                minWidth: window.innerWidth * 0.7, // ~70% of viewport width
-                title: "Visit Details",
+                width: window.innerWidth * 0.7, // ~70% of viewport width
+                title: "Job Details",
             });
         }
     });
