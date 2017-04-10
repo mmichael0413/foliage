eab2569fad740729b5f58a0430a6258bf601aea7
Alicia Sedlock <aliciasedlock@ThirdChannels-MacBook-Pro-2.local>
2017-04-10 11:42:25 -0400

    remove frontend components to export button

--- js/apps/thirdchannel/views/reports/field_activity/index/main.js
+++ js/apps/thirdchannel/views/reports/field_activity/index/main.js
@@ -1,22 +1,12 @@
 define(function(require) {
     var $ = require('jquery'),
         _ = require('underscore'),
-        ExportView = require('thirdchannel/views/utils/report_export_button'),
         FieldActivitiesReportView = require('thirdchannel/views/reports/field_activity/report'),
-        Filter = require('thirdchannel/views/filter/main'),
-        ReportExportModel = require('thirdchannel/models/reports/export');
+        Filter = require('thirdchannel/views/filter/main');
 
     return {
         init: function (options) {
-
-            $(".actions .export").each(function() {
-                var data = {baseUrl: "/programs/" + options.programId + "/reports/export"};
-                var model = new ReportExportModel(_.extend(data, options));
-                new ExportView({model: model}).render(this);
-            });
-
             Filter.init();
-
             new FieldActivitiesReportView(options).render();
         }
     };
