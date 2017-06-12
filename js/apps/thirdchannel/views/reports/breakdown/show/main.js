437b3dd0e5a97c8a0e2c12c28eaadf8e03f7b088
Mike Michaelevich <mmichael0413@gmail.com>
2017-06-12 10:09:56 -0400

    FAS-243: Add CSV export to KPIs report breakdowns (#239)

--- js/apps/thirdchannel/views/reports/breakdown/show/main.js
+++ js/apps/thirdchannel/views/reports/breakdown/show/main.js
@@ -1,10 +1,20 @@
 define(function(require) {
   var $ = require('jquery'),
-    ReportBreakdownView = require('thirdchannel/views/reports/breakdown/show/breakdown_list'),
-	Filter = require('thirdchannel/views/filter/main');
+      _ = require('underscore'),
+      ReportBreakdownView = require('thirdchannel/views/reports/breakdown/show/breakdown_list'),
+      Filter = require('thirdchannel/views/filter/main'),
+      ExportView = require('thirdchannel/views/utils/report_export_button'),
+      ExportModel = require('thirdchannel/models/reports/export');
 
   return {
     init: function (options) {
+      $('.actions .export').each(function() {
+          var data = {
+            baseUrl: '/programs/' + options.programId + '/reports/field_activities/breakdown/' + options.type + '/export'
+          };
+          var model = new ExportModel(_.extend(data, options));
+          new ExportView({ model: model }).render(this);
+      });
       new ReportBreakdownView(options).render();
       Filter.init();
     }
